61118696cf2db2266801f54aa79676d3e604dc54
andre-brandao <brandaoandre00@gmail.com>
2024-03-02 10:15:31 -0300

    gnome tweks and autologin

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -8,17 +8,17 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  # Enable automatic login for the user.
-  # services.xserver.displayManager.autoLogin.enable = true;
-  # services.xserver.displayManager.autoLogin.user = "andre";
+  Enable automatic login for the user.
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "andre";
 
-  # # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  # systemd.services."getty@tty1".enable = false;
-  # systemd.services."autovt@tty1".enable = false;
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
 
-  #environment.systemPackages = with pkgs [
-   # gnome3.gnome-tweaks
-  #];
+  environment.systemPackages = with pkgs [
+   gnome3.gnome-tweaks
+  ];
 
 }
