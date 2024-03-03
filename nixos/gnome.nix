af01c206080e67f5bf80e25344a3e325eefba6f8
andre-brandao <brandaoandre00@gmail.com>
2024-03-03 00:33:56 -0300

    new extensions

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -19,6 +19,12 @@
 
   environment.systemPackages = with pkgs; [
    gnome3.gnome-tweaks
+   gnomeExtensions.blur-my-shell
+   gnomeExtensions.gnome-40-ui-improvements
+   gnomeExtensions.vitals
+   gnomeExtensions.caffeine
+   gnomeExtensions.gnome-clipboard
+   gnomeExtensions.dash-to-dock
   ];
 
 }
