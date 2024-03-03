ce902186b4634a7e76766f73b2837d56efc63625
andre-brandao <brandaoandre00@gmail.com>
2024-03-03 01:06:14 -0300

    libvirtd update

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -2,7 +2,21 @@
 {pkgs, ...}:
 
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+  enable = true;
+  qemu = {
+    package = pkgs.qemu_kvm;
+    runAsRoot = true;
+    swtpm.enable = true;
+    ovmf = {
+      enable = true;
+      packages = [(pkgs.unstable.OVMF.override {
+        secureBoot = true;
+        tpmSupport = true;
+      }).fd];
+    };
+  };
+  };
   programs.virt-manager.enable = true;
   dconf.settings = {
   "org/virt-manager/virt-manager/connections" = {
