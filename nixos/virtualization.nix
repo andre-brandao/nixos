cb1436a02c15ebd8a088f77c415e9dd5d47d3301
andre-brandao <brandaoandre00@gmail.com>
2024-03-03 01:18:20 -0300

    + dconf

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -1,14 +1,14 @@
 
-{pkgs, ...}:
+{pkgs,dconf, ...}:
 
 {
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
-  # dconf.settings = {
-  # "org/virt-manager/virt-manager/connections" = {
-  #   autoconnect = ["qemu:///system"];
-  #   uris = ["qemu:///system"];
-  #   };
-  # };
+  dconf.settings = {
+  "org/virt-manager/virt-manager/connections" = {
+    autoconnect = ["qemu:///system"];
+    uris = ["qemu:///system"];
+    };
+  };
 
 }
