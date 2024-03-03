9eaabd82ab750b05e19de8240fcffc7bdc09b3e0
andre-brandao <brandaoandre00@gmail.com>
2024-03-03 02:28:45 -0300

    docker

--- nixos/users.nix
+++ nixos/users.nix
@@ -7,7 +7,7 @@
     andre = {
     isNormalUser = true;
     description = "Andre Brandao";
-    extraGroups = [ "networkmanager" "wheel" "libvirtd"];
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker"];
     shell = pkgs.zsh;
     
     packages = with pkgs; [
--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -2,6 +2,8 @@
 {pkgs, ...}:
 
 {
+  virtualisation.docker.enable = true;
+
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
 }
