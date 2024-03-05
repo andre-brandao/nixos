78b364ae51ea5e424c74468f1f3269593b82d129
andre-brandao <brandaoandre00@gmail.com>
2024-03-05 13:05:36 -0300

    docker helper

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -2,8 +2,11 @@
 {pkgs, ...}:
 
 {
+  ## DOCKER
   virtualisation.docker.enable = true;
+  environment.systemPackages = with pkgs; [lazydocker]
 
+  ## QEMU + VirtManager
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
 }
