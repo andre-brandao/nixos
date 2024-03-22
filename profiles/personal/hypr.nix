e7bf919b3b7f2baf4ed5974c32bdc595d78665d0
andre-brandao <brandaoandre00@gmail.com>
2024-03-22 18:53:53 -0300

    home-manager hyprland config

--- profiles/personal/hypr.nix
+++ profiles/personal/hypr.nix
@@ -4,23 +4,161 @@
 
 let
   startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
-    ${(pkgs.waybar.override {
-    wireplumberSupport = false;
-    })}/bin/waybar &
+    ${(pkgs.waybar.override { wireplumberSupport = false; })}/bin/waybar &
 
     ${pkgs.swww}/bin/swww init &
-  
+
     ${pkgs.dunst}/bin/dunst 
 
       
   '';
-in
-{
+in {
+  # programs.hyprland.enable = true
+  # programs.hyperland.packege = inputs.hyprland.packages."${pkgs.system}".hyprland
   wayland.windowManager.hyprland = {
     enable = true;
 
     settings = {
-      exec-once = ''${startupScript}/bin/start'';
+      ## See https://wiki.hyprland.org/Configuring/Monitors/
+      monitor = "DP-1, 1920x1200, 0x0, 1";
+
+      # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
+      input = {
+        kb_layout = "us";
+        kb_variant = "";
+        #  kb_model =
+        #  kb_options =
+        #  kb_rules =
+
+        follow_mouse = 1;
+
+        touchpad = { natural_scroll = "yes"; };
+
+        sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+      };
+
+      general = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+        gaps_in = 5;
+        gaps_out = 20;
+        border_size = 2;
+        col = {
+
+          inactive_border = "rgba(595959aa)";
+          active_border = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        };
+        layout = "dwindle";
+
+        # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+        allow_tearing = false;
+      };
+
+      decoration = {
+        # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+        rounding = 10;
+
+        blur = {
+          enabled = true;
+          size = 3;
+          passes = 1;
+        };
+
+        drop_shadow = "yes";
+        shadow_range = 4;
+        shadow_render_power = 3;
+        "col.shadow" = "rgba(1a1a1aee)";
+      };
+
+      animations = {
+        enabled = "yes";
+
+        # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+        animation = [
+
+          "windows, 1, 7, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+
+          "borderangle, 1, 8, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
+
+      master = {
+        #  # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
+        new_is_master = true;
+      };
+      dwindle = {
+        #   # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
+        pseudotile = "yes";
+        preserve_split = "yes";
+      };
+      gestures = {
+        # # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        workspace_swipe = true;
+      };
+
+      misc = {
+        #  # See https://wiki.hyprland.org/Configuring/Variables/ for more
+        force_default_wallpaper = -1;
+      };
+
+      "$mainMod" = "SUPER";
+      bind = [
+        "$mainMod, Q, exec, alacritty"
+        "$mainMod, C, killactive"
+        "$mainMod, M, exit"
+        "$mainMod, E, exec, dolphin"
+        "$mainMod, V, togglefloating"
+        "$mainMod, R, exec, wofi --show drun"
+        "$mainMod, P, pseudo" # dwindle
+        "$mainMod, J, togglesplit" # dwindle
+        "$mainMod, S, exec, rofi -show drun -show-icons"
+
+        #WORKSPACE SWITCH
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 6, workspace, 6"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
+
+        #MOVE CURRENT WINDOW TO WORKSPACE
+        "$mainMod SHIFT, 1, movetoworkspace, 1"
+        "$mainMod SHIFT, 2, movetoworkspace, 2"
+        "$mainMod SHIFT, 3, movetoworkspace, 3"
+        "$mainMod SHIFT, 4, movetoworkspace, 4"
+        "$mainMod SHIFT, 5, movetoworkspace, 5"
+        "$mainMod SHIFT, 6, movetoworkspace, 6"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 1"
+
+        # Example special workspace (scratchpad)
+        "$mainMod, S, togglespecialworkspace, magic"
+        "$mainMod SHIFT, S, movetoworkspace, special:magic"
+
+        # Scroll through existing workspaces with mainMod + scroll
+        "$mainMod, mouse_down, workspace, e+1"
+        "$mainMod, mouse_up, workspace, e-1"
+
+        # Move/resize windows with mainMod + LMB/RMB and dragging
+        "$mainMod, mouse:272, movewindow"
+        "$mainMod, mouse:273, resizewindow"
+      ];
+
+      exec-once = "${startupScript}/bin/start";
     };
   };
 }
