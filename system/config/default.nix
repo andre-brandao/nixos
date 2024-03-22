704d2e9656fac46985860ff7678aa75ad43d5000
andre-brandao <brandaoandre00@gmail.com>
2024-03-21 21:02:54 -0300

    nixos-conf-editor

--- system/config/default.nix
+++ system/config/default.nix
@@ -1,4 +1,12 @@
-{ pkgs, ... }: {
+{ config, pkgs, lib, ... }:
+let
+  nixos-conf-editor = import (pkgs.fetchFromGitHub {
+    owner = "snowfallorg";
+    repo = "nixos-conf-editor";
+    rev = "0.1.2";
+    sha256 = "sha256-/ktLbmF1pU3vFHeGooDYswJipNE2YINm0WpF9Wd1gw8=";
+  }) { };
+in {
   imports = [
     ./bootloader.nix
     ./firewall.nix
@@ -9,5 +17,9 @@
     ./gc.nix
   ];
 
-  environment.systemPackages = with pkgs; [ nixos-conf-editor ];
+  environment.systemPackages = with pkgs;
+    [
+      nixos-conf-editor
+      # rest of your packages
+    ];
 }
