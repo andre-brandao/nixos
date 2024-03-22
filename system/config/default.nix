9e50335ce8d7e61280cb37f904890f6829877fbc
andre-brandao <brandaoandre00@gmail.com>
2024-03-21 21:24:33 -0300

    removed gui editor

--- system/config/default.nix
+++ system/config/default.nix
@@ -1,12 +1,4 @@
-{ config, pkgs, lib, ... }:
-let
-  nixos-conf-editor = import (pkgs.fetchFromGitHub {
-    owner = "snowfallorg";
-    repo = "nixos-conf-editor";
-    rev = "0.1.2";
-    sha256 = "sha256-/ktLbmF1pU3vFHeGooDYswJipNE2YINm0WpF9Wd1gw8=";
-  }) { };
-in {
+{  ... }:{
   imports = [
     ./bootloader.nix
     ./firewall.nix
@@ -16,10 +8,4 @@ in {
     ./printer.nix
     ./gc.nix
   ];
-
-  environment.systemPackages = with pkgs;
-    [
-      nixos-conf-editor
-      # rest of your packages
-    ];
 }
