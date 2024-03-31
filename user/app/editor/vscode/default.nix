6be1814682f24a9373fa5f4adc5f6bbd8b279446
andre-brandao <brandaoandre00@gmail.com>
2024-03-31 15:00:26 -0300

    codium

--- user/app/editor/vscode/default.nix
+++ user/app/editor/vscode/default.nix
@@ -1,7 +1,18 @@
 { config, pkgs, inputs, ... }: {
 
-  #programs.vscode = {
-  # enable = true;
-  #};
-  home.packages = [ pkgs.vscode ];
+  programs.vscode = {
+    enable = true;
+    # open source version of vscode
+    package = pkgs.vscodium.fhs;
+    userSettings = {
+      "files.autoSave" = "off";
+      "window.titleBarStyle" = "custom";
+      "workbench.sideBar.location" = "right";
+      "editor.renderWhitespace" = "boundary";
+    };
+  };
+  home.packages = [
+    #microsoft vscode
+    pkgs.vscode
+  ];
 }
