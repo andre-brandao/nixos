cff262221c12a2a146f50460a5477b90e13f8ab8
andre-brandao <brandaoandre@gmail.com>
2024-05-03 14:01:23 -0300

    removed firmware update

--- hosts/samsumg-ebook20/configuration.nix
+++ hosts/samsumg-ebook20/configuration.nix
@@ -92,9 +92,6 @@
     git
   ];
 
-  # firmware updater
-  services.fwupd.enable = true;
-
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   # services.openssh = {
