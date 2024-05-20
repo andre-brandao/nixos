db00a94a36bee87bcf96b1600902630f7bad3b2f
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2024-05-20 11:48:11 -0300

    terminal configs

--- user/app/terminal/kitty.nix
+++ user/app/terminal/kitty.nix
@@ -1,9 +1,7 @@
 { pkgs, lib, ... }:
 
 {
-  home.packages = with pkgs; [
-    kitty
-  ];
+  home.packages = with pkgs; [ kitty ];
   programs.kitty.enable = true;
   programs.kitty.settings = {
     background_opacity = lib.mkForce "0.75";
