49def35f92b27473aa77106c9bdf92159f0793d8
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2024-05-20 11:29:12 -0300

    terminal settings

--- user/app/terminal/kitty.nix
+++ user/app/terminal/kitty.nix
@@ -1,18 +1,11 @@
 { pkgs, lib, ... }:
-{
-  home.packages = with pkgs; [ kitty ];
-  programs.kitty = {
-    enable = true;
-    font = {
-      name = "JetBrainsMono Nerd Font";
-      size = 10;
-    };
 
-    # shellIntegration.enableFishIntegration = true;
-    theme = "Catppuccin-Macchiato";
-    settings = {
-      background_opacity = lib.mkForce "0.55";
-      background_opacity_when_focused = lib.mkForce "0.75";
-    };
+{
+  home.packages = with pkgs; [
+    kitty
+  ];
+  programs.kitty.enable = true;
+  programs.kitty.settings = {
+    background_opacity = lib.mkForce "0.75";
   };
 }
