41bb7c1021ef3851a7c10a28473557047fa7df7d
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2024-06-10 12:26:10 -0300

    distrobox

--- user/app/virtualization/distrobox.nix
+++ user/app/virtualization/distrobox.nix
@@ -1,104 +1,66 @@
-{ pkgs, config, ... }:
-with builtins;
-let
-  mkHome = box: ".local/share/distrobox/${box}";
+{
+  pkgs,
+  config,
+  ...
+}: {
+  imports = [./modules/distrobox.nix];
 
-  boxes = {
-    fedora = {
-      home = mkHome "Fedora";
-      alias = "fedora";
-      img = "quay.io/fedora/fedora:rawhide";
-    };
-    ubuntu = {
-      home = mkHome "Ubuntu";
-      alias = "ubuntu";
-      img = "docker.io/library/ubuntu:22.04";
-    };
-    arch = {
-      home = mkHome "Arch";
-      alias = "arch";
-      img = "docker.io/library/archlinux:latest";
-    };
-  };
+  programs.distrobox = {
+    enable = true;
 
-  mkBoxLinks =
-    box:
-    let
-      ln = config.lib.file.mkOutOfStoreSymlink;
-      links = [
+    boxes = let
+      exec = "${pkgs.nushell}/bin/nu";
+      symlinks = [
         ".bashrc"
         ".zshrc"
         ".config/nushell"
         ".config/nvim"
         ".config/nix"
         ".config/starship.toml"
-        ".local/"
-        ".cache/"
       ];
-    in
-    listToAttrs (
-      map (link: {
-        name = "${box.home}/${link}";
-        value = {
-          source = ln "${config.home.homeDirectory}/${link}";
-        };
-      }) links
-    );
-
-  mkBoxAlias =
-    let
-      db = "${pkgs.distrobox}/bin/distrobox";
-    in
-    box:
-    pkgs.writeShellScriptBin box.alias ''
-      if ! ${db} list | grep ${box.alias}; then
-          ${db} create \
-            --name "${box.alias}" \
-            --home ${config.home.homeDirectory}/${box.home} \
-            --image "${box.img}"
-      fi
-
-      ${db} enter ${box.alias}
-    '';
-
-  path = [
-    "/bin"
-    "/sbin"
-    "/usr/bin"
-    "/usr/sbin"
-    "/usr/local/bin"
-    "/usr/sbin"
-    "${pkgs.nix}/bin"
-  ];
-in
-{
-  home.packages =
-    let
-      aliases = mapAttrs (name: value: mkBoxAlias value) boxes;
-    in
-    (attrValues aliases) ++ [ pkgs.distrobox ];
-
-  home.file =
-    let
-      links = mapAttrs (name: value: mkBoxLinks value) boxes;
-    in
-    foldl' (x: y: x // y) { } (attrValues links);
-
-  programs.bash.initExtra = ''
-    if [[ -f "/run/.containerenv" ]]; then
-      export PATH="${concatStringsSep ":" path}"
-    fi
-  '';
-
-  programs.zsh.initExtra = ''
-    if [[ -f "/run/.containerenv" ]]; then
-      export PATH="${concatStringsSep ":" path}"
-    fi
-  '';
-
-  programs.nushell.extraConfig = ''
-    if ("/run/.containerenv" | path exists) {
-      $env.PATH = [${concatStringsSep " " (map (x: "${x}") path)}]
-    }
-  '';
+      packages =
+        config.packages.cli
+        ++ [
+          pkgs.nix
+          pkgs.git
+        ];
+    in {
+      # Alpine = {
+      #   inherit exec symlinks;
+      #   img = "docker.io/library/alpine:latest";
+      #   packages = "git neovim";
+      # };
+      Fedora = {
+        inherit exec symlinks;
+        packages = "nodejs npm poetry gcc mysql-devel python3-devel wl-clipboard";
+        img = "registry.fedoraproject.org/fedora-toolbox:rawhide";
+        nixPackages =
+          packages
+          ++ [
+            (pkgs.writeShellScriptBin "pr" "poetry run $@")
+            (pkgs.writeShellScriptBin "prpm" "poetry run python manage.py $@")
+          ];
+      };
+      Arch = {
+        inherit exec symlinks;
+        img = "docker.io/library/archlinux:latest";
+        packages = "base-devel wl-clipboard";
+        nixPackages =
+          packages
+          ++ [
+            (pkgs.writeShellScriptBin "yay" ''
+              if [[ ! -f /bin/yay ]]; then
+                tmpdir="$HOME/.yay-bin"
+                if [[ -d "$tmpdir" ]]; then sudo rm -r "$tmpdir"; fi
+                git clone https://aur.archlinux.org/yay-bin.git "$tmpdir"
+                cd "$tmpdir"
+                makepkg -si
+                sudo rm -r "$tmpdir"
+              fi
+              /bin/yay $@
+            '')
+          ];
+      };
+    };
+  };
 }
