419858ac7e65de7f169c4d35cdd5e4f4a66a73db
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-01-07 00:52:07 -0300

    ghostty cfg

---
+++ user/app/terminal/ghostty.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+{
+  home.file.".config/ghostty/config".text = ''
+    theme = deep
+    # window-theme = ghostty
+    background-opacity = 0.8
+    window-decoration = false
+
+
+    ## KEYBINDS
+
+    keybind = ctrl+-=new_split:down
+
+    keybind = ctrl+\=new_split:right
+    keybind = ctrl+]=new_split:right
+
+    keybind = ctrl+up=goto_split:top
+    keybind = ctrl+down=goto_split:bottom
+    keybind = ctrl+left=goto_split:left
+    keybind = ctrl+right=goto_split:right
+
+    keybind = ctrl+w=close_surface
+
+    keybind = ctrl+t=new_tab
+    keybind = ctrl+n=next_tab
+    keybind = ctrl+shift+n=previous_tab
+
+    keybind = ctrl+shift+space=toggle_tab_overview
+
+  '';
+}
