596c29fe77eb8590b5211393819b4a1aa0300ebc
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-02-25 20:56:34 -0300

    pip rules

--- user/desk-env/hyprland/rules.nix
+++ user/desk-env/hyprland/rules.nix
@@ -45,6 +45,13 @@
       "rounding 0, floating:0, onworkspace:w[tv1]"
       "bordersize 0, floating:0, onworkspace:f[1]"
       "rounding 0, floating:0, onworkspace:f[1]"
+
+      # PIP
+      "float, title:^Picture-in-Picture$"
+      "pin, title:^Picture-in-Picture$"
+      # "move 100%-w-20 100%-h-20, title:^Picture-in-Picture$"
+      "noinitialfocus, title:^Picture-in-Picture$"
+      "move 69% 69%, title:^Picture-in-Picture$"
     ];
 
   };
