58f31c0dd23da725de35ff8c3084cb59ba4632ec
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-03-10 09:08:43 -0300

    flake update + zen update

--- packages/zen.nix
+++ packages/zen.nix
@@ -5,7 +5,8 @@ let
 
   src = fetchurl {
     url = "https://github.com/zen-browser/desktop/releases/latest/download/zen-x86_64.AppImage";
-    sha256 = "sha256-1s5T9adpo55cLk+ECaiYtZCEQtuAkmr7/4IuxRfsL4U=";
+    # sha256 = "sha256-1s5T9adpo55cLk+ECaiYtZCEQtuAkmr7/4IuxRfsL4U=";
+    sha256 = "sha256-hZiJ8JLzLhtD1W8DAso3yBAJYhFE+nJEbQJa59AWjnU=";
   };
 
   appimageContents = appimageTools.extract {
