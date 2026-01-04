5c3b3b0d9a11e6d92a88f9e3683e6768ebed9545
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2026-01-04 13:50:43 -0300

    init templates

---
+++ templates/latexmk/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A simple LaTeX template for writing documents with latexmk";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs (import systems);
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (system: {
+        default = pkgs.${system}.callPackage ./default.nix { };
+      });
+    };
+}
---
+++ templates/python/flake.nix
@@ -0,0 +1,47 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    systems.url = "github:nix-systems/default";
+
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      poetry2nix,
+    }:
+    let
+
+      forAllSystems = nixpkgs.lib.genAttrs (import systems);
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
+        in
+        {
+          default = mkPoetryApplication { projectDir = self; };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryEnv;
+        in
+        {
+          default = pkgs.${system}.mkShellNoCC {
+            packages = with pkgs.${system}; [
+              (mkPoetryEnv { projectDir = self; })
+              poetry
+            ];
+          };
+        }
+      );
+    };
+}
